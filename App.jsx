1a0b8db76ce01ee92d84ed84e44b3a196631748e
Khushi1904-ai <teeny1904@gmail.com>
2026-08-13 19:46:07 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Paradise Nursery</h1>
+      <button onClick={() => alert('Welcome to Paradise Nursery!')}>
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default App;
